7804d3230097643f21930ccd7c837e8325dafd16
EddieOrozco <orozcoe230@gmail.com>
2020-03-26 18:47:06 -0500

    Styling components

--- src/components/Listings/Listings.js
+++ src/components/Listings/Listings.js
@@ -1,14 +1,16 @@
 import React, { Component } from "react";
 
+import "./Listings.scss";
+
 class Listings extends Component {
   render() {
     return (
-      <section className="listings">
+      <section id="listings">
         <section className="search-area">
           <input type="text" name="search" />
         </section>
         <section className="sortby-area">
-          <div>390 results found</div>
+          <div className="results">390 results found</div>
           <div className="sort-options">
             <select name="sortby" className="sortby">
               <option value="price-asc">Highest Price</option>
